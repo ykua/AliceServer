ceb5f90f586e83d423611cf4844032bbbddb8b10
Yuichi Kageyama <yuichitech@gmail.com>
2023-09-25 22:55:01 +0900

    mongoの初期設定バグを修正

--- docker/mongo/initdb.d/init-mongo.js
+++ docker/mongo/initdb.d/init-mongo.js
@@ -1,6 +1,6 @@
 db = db.getSiblingDB('alice');  // aliceデータベースを参照
 // daqコレクションが存在しない場合のみ作成
-if (!db.getCollecitonNames().includes('daq')) {
+if (!db.getCollectionNames().includes('daq')) {
     db.createCollection('daq');
 }
 
@@ -11,13 +11,13 @@ db.daq.insertMany([
         data_type: "temperature",
         data_category: "sensor",
         daq_value: 0.1,
-        date: ISODate("2023-09-23T00:00:00Z")
+        date: ISODate("2023-09-23T00:00:00Z"),
     },
     {
         node_number: "test-001",
         data_type: "temperature",
         data_category: "sensor",
         daq_value: 0.25,
-        date: ISODate("2023-09-23T00:00:01Z")
+        date: ISODate("2023-09-23T00:00:01Z"),
     },
 ]);
